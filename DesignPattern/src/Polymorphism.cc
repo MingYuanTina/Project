3a976abeeb67280abd87055dc45cb0d31433ce1c
MingYuanTina <bjteana@gmail.com>
2018-04-30 22:52:28 -0400

    polymorphism file

--- DesignPattern/src/Polymorphism.cc
+++ DesignPattern/src/Polymorphism.cc
@@ -1,5 +1,6 @@
 // Resources:
 //	http://www.catonmat.net/blog/cpp-polymorphism/
+//  https://www.hackerearth.com/practice/notes/virtual-function-and-virtual-table-in-c/
 
 // These polymorphisms also go by different names in C++,
 
@@ -26,6 +27,9 @@ using namespace std;
 /***************************** Subtyping Polymorphism *****************************/
 /*  Description:
  *	- Use derived class through base class pointers or reference
+ *  - Also called runtime polymorphism since it occurs at runtime
+ *  - v_ptr is created to the base class that contains virtual functions
+ *  - vtable is created compile time v_ptr holds the address of v_table of the corresponding class
  */
 
 /*  Example:
@@ -51,9 +55,41 @@ public:
 	}
 };
 
+/***************************** Parametric Polymorphism *****************************/
+/* Description:
+ * - provide a means to execute the same code for any type using template
+ * - build generic methods (binding types with shared functionality)
+ * - compile time polymorphism
+ */
+template <class T>
+T max(T a, T b) {
+ return a > b ? a : b;
+}
+
+template <class T>
+T add(T a, T b) {
+	return a + b;
+}
+
+/***************************** Ad-hoc Polymorphism *****************************/
+/* Description:
+ * - Ad-hoc polymorphism allows functions with the same name act differently for each type
+ * - build generic methods (binding function interface with different functionality)
+ * - runtime polymorphism
+ */
+const char *max(const char *a, const char *b) {
+ return strcmp(a, b) > 0 ? a : b;
+}
+
+/***************************** Coercion Polymorphism *****************************/
+/* Description:
+ * - Casting
+ */
 
 int main(){
 	Shape *obj1 = new Rectangle(10, 10);
 	cout << "The area of Rectangle is: " << obj1->getArea() << endl;
+	cout << ::max(5,9) << endl;
+	cout << ::max("A", "B") << endl;
 	return 0;
 }
